bfa036a5fa06bd66b332b264568dd35c58a200a9
ZuraDrobzhevi <zuradrobzhevi@icloud.com>
2024-12-23 15:45:52 -0800

    second commit

---
+++ level 25/classwork 25/level.1.py
@@ -0,0 +1,24 @@
+
+
+
+
+#.insert-ის დახმარებით ჩვენ ვანაცვლებთ მონაცემებს.
+names=["vano","giorgi","nika"]
+names.insert(1, "dato")
+print(names)
+
+#len ფუნქცია ჩვენ გამოგვიტანს თუ რამდენი სიმბოლოა სტრინგშ.მასში ჩვენ შეგვიძლია ჩავწეროთ ცვლადი ან სხვა მონაცემებიც რომელიც გინდათ. 
+name="jafibbanj$^&*_sniasjdu@@ian!sjdnas jndjaind iua???nia"
+print(len(name))
+
+#append-ის დახმარებით ჩვენ შეგვიძლია ცხრილს მივუმატოთ ახალი მონაცემი.
+roro = ["Zura","Renee","ZU",]
+roro.append("REN")
+print(roro)
+
+
+#pop-კონკრეტულსიის ელემენტს ამოაგდებს სიიდან
+roro = ["Zura","Renee","ZU",]
+roro.pop(2)
+print(roro)
+
---
+++ level 25/classwork 25/level.3.py
@@ -0,0 +1,9 @@
+#3)შექმენით სია რომელიც შედგება 5 ელემენტისგან და შემდეგ pop მეთოდის გამოყენებით ამოაკელით სიის პირველი და ბოლო ელემენტი.
+
+#kratos list
+
+to_get_my_revenge=["Zeus","Athena","Helios","Thanatos","Poseidon"]
+to_get_my_revenge.pop(2)
+print(to_get_my_revenge)
+
+#going to the next one.
