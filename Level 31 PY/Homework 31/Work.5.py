ba6b846f7924c591eac82f00b9725362cdef7efb
ZuraDrobzhevi <zuradrobzhevi@icloud.com>
2025-05-31 18:20:24 -0700

    second commit

--- Level 31 PY/Homework 31/Work.5.py
+++ Level 31 PY/Homework 31/Work.5.py
@@ -2,13 +2,15 @@
 # 5) ცვლადში შეინახეთ სახელი. შემდეგ მომხმარებელსაც შემოატანინეთ თავისი სახელი. თუ თქვენი სახელების პირველი და ბოლო ასო ერთმანეთს დაემთხვევა გამოიტანეთ 2.
 # თუ სახელის ან პირველი ან ბოლო ასო დაემთხვევა გამოიტანეთ 1. ყველა სხვა შემთხვევაში: 0.
 
-Name="Zura"
-d=Name.find("0")
-You_Name=input("wat is your name?:")
 
-
-print(d)
-
+my_name = "Zura"
+user_name = input("what is your name?:")
+if my_name[0] == user_name[0] and my_name[-1] == user_name[-1]:
+    print(2)
+elif my_name[0] == user_name[0] or my_name[-1] == user_name[-1]:
+    print(1)
+else:
+    print(0)
 
 
 
