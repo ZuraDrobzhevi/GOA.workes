67781ca5f4c3a1ff0aaf85d87eb2a883ca8420ad
ZuraDrobzhevi <zuradrobzhevi@icloud.com>
2025-06-29 20:18:50 -0700

    second commit

---
+++ Level 33 PY/Classwork 33/Work.2.py
@@ -0,0 +1,10 @@
+#2)შექმენით ფუნქცია რომელსაც ექნება ერთი პარამეტრი "სახელი" და მიესალმეთ მომხმარებელს
+
+
+
+name = input("What is your name? : ")
+
+def hello(name):
+    print("Hello " + name)
+
+hello(name)
