0d8a332d3fab1279afeb8275a4fb54c947cf9334
ZuraDrobzhevi <zuradrobzhevi@icloud.com>
2025-07-04 14:24:18 -0700

    second commit

--- Level 33 PY/Classwork 33/Work.3.py
+++ Level 33 PY/Classwork 33/Work.3.py
@@ -1,1 +1,12 @@
-# მომხმარებელს 5-ჯერ შეაყვანინე ტექსტი, ამოწმე რომ შეტანილი ტექსტის სიგრძე იყოს მეტი 0-ზე და თითოეული ტექსტი დაამატე სიაში. ბოლოს დაამატე სიაში ასევე "საბოლოო" ცვლადი.
+#3)შექმენით სია სადაც შეინახავთ 5 სახელს. შემდეგ მომხმარებელს შემოატანინეთ სახელი და ინდექსი თუ ინდექსი აღემატება სიის რაოდენობას დააბრუნეთ ერორი სტრინგის სახით სხვაშემთხვევაში დაამატეთ სიაში.
+
+names = ["Ana", "Levan", "Mariam", "Giorgi", "Nino"]
+
+new_name = input("Enter a new name: ")
+index = int(input("Enter index: "))
+
+if index > len(names):
+    print("Error: Index exceeds list size")
+else:
+    names.insert(index, new_name)
+    print("Updated list:", names)
--- Level 33 PY/Classwork 33/Work.5.py
+++ Level 33 PY/Classwork 33/Work.5.py
@@ -1,1 +1,17 @@
 # 5)შექმენით ფუნქცია, რომელსაც გადაეცემა წინადადება (მაგ: "hello how are you") და შედეგად აბრუნებს სიას, სადაც იქნება მოცემული თითოეული სიტყვა და გვერძე ეწერება მისი სიგრძე (["hello 5", "how 3", "are 3", "you 3"]
+
+def word_len(sentance):
+    result = []
+    words = ""
+
+    for i in sentance:
+        if i != " ":
+            words += i
+        else:
+            result.append(words + str(len(words)))
+            words = ""
+
+    result.append(words + str(len(words)))
+    print(result)
+
+word_len("Hello how are you")
